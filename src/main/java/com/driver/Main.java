42cd23919e083d4a4c99d40803fc7d7594464e2d
Praveen <praveenreddynagilla1996@gmail.com>
2023-05-17 07:44:31 +0530

    solving overriding

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,23 +4,23 @@ public class Main {
     public static void main(String[] args) {
         //creating an object of B and calling the method of A in it
         B obj= new B();
-        String class1=obj.meth(); //calling A class method but it got overridden already.
-        String class2=obj.meth(); //calling B class Method
-        System.out.println(class1);
-        System.out.println(class2);
+        obj.meth(); //calling A class method but it got overridden already.
+        obj.meth(); //calling B class Method
+
     }
 }
  class A {
-    public String meth()
+    public void meth()
     {
-        return "Invoking method from class A";
+
+        System.out.println("Invoking method from class A");
     }
 }
 class B extends A{
 
-    public String meth () {  //overriding the method of class A.
+    public void meth () {  //overriding the method of class A.
 
-        return "Method is overridden in Extended class B";
+        System.out.println("Method is overridden in Extended class B");
     }
 
 }
